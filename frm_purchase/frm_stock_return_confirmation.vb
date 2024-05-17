c2fb70fec9fa8b8af680117fe5935fb29f99ccf9
jz3n <johnzenery@gmail.com>
2024-05-18 01:24:33 +0800

    Changed Supplier Confirmation (Return to Vendor) controls and added restriction for with batch RIDs.

--- frm_purchase/frm_stock_return_confirmation.vb
+++ frm_purchase/frm_stock_return_confirmation.vb
@@ -1,4 +1,9 @@
-﻿Imports DevExpress.XtraRichEdit.Import.OpenXml
+﻿Imports System.ComponentModel
+Imports DevExpress.Data.Browsing
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraGrid.Views.Base
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraRichEdit.Import.OpenXml
 Imports MySqlConnector
 
 Public Class frm_stock_return_confirmation
@@ -16,7 +21,7 @@ Public Class frm_stock_return_confirmation
         Try
             conn.Open()
             'GET UNITS FROM RETRUNS
-            Using cmd = New MySqlCommand($"SELECT qty, ims_inventory.pid, ims_inventory.winmodel, ims_inventory.supmodel, serial, rs_returns.type, issue, rs_returns.id, approval_supplier as status
+            Using cmd = New MySqlCommand($"SELECT qty, ims_inventory.pid, ims_inventory.winmodel, ims_inventory.supmodel, serial, rs_returns.type, issue, rs_returns.id, approval_supplier as status, batch_id
                                             FROM rs_returns 
                                             LEFT JOIN ims_inventory ON ims_inventory.pid=rs_returns.pid
                                             WHERE NOT approval_supplier='Pending' AND pullout_id={id}", conn)
@@ -117,6 +122,54 @@ Public Class frm_stock_return_confirmation
             End If
         Else
             MsgBox("No selected rows!", vbCritical, "Not Found")
+        End If
+    End Sub
+
+    'COLUMN STATUS : CBB
+    Private Sub btn_submit_Click(sender As Object, e As EventArgs) Handles btn_submit.Click
+        Try
+            conn.Open()
+            Dim trans = conn.BeginTransaction
+
+            Try
+                Using cmd = New MySqlCommand("UPDATE rs_returns 
+                                            SET approval_supplier=@status 
+                                            WHERE id=@id", conn, trans)
+                    cmd.Parameters.AddWithValue("@id", 0)
+                    cmd.Parameters.AddWithValue("@status", Nothing)
+                    cmd.Prepare()
+
+                    For i = 0 To grid_returns_view.RowCount - 1
+                        cmd.Parameters("@id").Value = grid_returns_view.GetRowCellValue(i, col_id)
+                        cmd.Parameters("@status").Value = grid_returns_view.GetRowCellValue(i, col_status)
+                        cmd.ExecuteNonQuery()
+                    Next
+                End Using
+
+                trans.Commit()
+                MsgBox("Submitted!", vbInformation, "Information")
+
+            Catch ex As MySqlException
+                trans.Rollback()
+                MsgBox(ex.Message, vbCritical, "Error")
+            End Try
+        Catch ex As Exception
+            MsgBox($"An error occured: {vbCrLf & vbCrLf & ex.Message}", vbCritical, "Error")
+        Finally
+            conn.Close()
+            RetrieveReturns(txt_pr_id.EditValue)
+        End Try
+    End Sub
+
+    Private Sub grid_returns_view_RowCellStyle(sender As Object, e As RowCellStyleEventArgs) Handles grid_returns_view.RowCellStyle
+        If Not IsDBNull(grid_returns_view.GetRowCellValue(e.RowHandle, col_batch)) Then
+            e.Appearance.BackColor = Color.LightGray
+        End If
+    End Sub
+
+    Private Sub grid_returns_view_ShowingEditor(sender As Object, e As CancelEventArgs) Handles grid_returns_view.ShowingEditor
+        If Not IsDBNull(grid_returns_view.GetFocusedRowCellValue(col_batch)) Then
+            e.Cancel = True
         End If
     End Sub
 End Class
